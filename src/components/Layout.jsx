edcc71e06311fa9d3410dc2692dda493caf5828b
frantan <francinetaing13@gmail.com>
2025-11-14 22:08:52 +0700

    style: add padding for scrollbar in main container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,18 +12,18 @@ function Layout() {
                 </section>
                 <section className="flex items-center gap-3 text-sm">
                     <a href="" className="px-5 py-2 bg-gray-100 rounded-xl transition hover:bg-green-200">breadcrumbs</a>
-                    <p>></p>
+                    <p>&gt;</p>
                     <a href="" className="px-5 py-2 bg-gray-100 rounded-xl transition hover:bg-green-200">hello</a>
-                    <p>></p>
+                    <p>&gt;</p>
                     <a href="" className="px-5 py-2 bg-gray-100 rounded-xl transition hover:bg-green-200">world</a>
                 </section>
             </nav>
 
             {/* HOMEPAGE SECTION */}
-            <section className="flex flex-col gap-10 h-[70vh] p-5 bg-gray-50 rounded-xl">
+            <section className="flex flex-col gap-10 h-[70vh] p-5 bg-gray-100 rounded-xl">
                 <div className="overflow-y-scroll h-full rounded-xl">
                     <h1>Hello, world!</h1>
-                    <div className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-5 pr-5">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>                    
