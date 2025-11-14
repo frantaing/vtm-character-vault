76a3055a98a96b6a470d5c6a6346d5a14ed044c2
frantan <francinetaing13@gmail.com>
2025-11-15 00:49:31 +0700

    feat: create main container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { NavBar } from './';
+import { NavBar, ContentBox } from './';
 
 function Layout() {
     return(
@@ -7,33 +7,12 @@ function Layout() {
             {/* insert navigation here! */}
             <NavBar></NavBar>
 
-            {/* HOMEPAGE SECTION */}
-            <section className="flex flex-col gap-10 h-[80vh] p-5 bg-gray-100 rounded-xl">
-                <div className="overflow-y-scroll h-full rounded-xl">
-                    <h1>Hello, world!</h1>
-                    <div className="flex flex-col gap-5 pr-5">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>                    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>                    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>                    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum explicabo esse inventore molestias, eligendi repellendus expedita? Incidunt dolores voluptates deserunt suscipit modi recusandae officia sit omnis dicta? Consectetur, quo?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>         
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, unde! Eum, amet dolor. Totam quibusdam velit aliquam nisi. Expedita aspernatur velit veniam, delectus fuga voluptatem. Quidem asperiores deleniti nisi at!</p>    
-                    </div>                    
-                </div>
+            <main>
+                {/* insert ContentBox main container here! */}
+                <ContentBox></ContentBox>       
+            </main>
 
-            </section>
+            
 
         </div>
     );
