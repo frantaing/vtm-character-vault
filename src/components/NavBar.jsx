56a96b538a70523cea1e9d821d38b9ac16ecbca9
frantan <francinetaing13@gmail.com>
2025-11-15 21:21:36 +0700

    style: test HomePage styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
             <section>
                 <div className="flex justify-between items-center pl-5 pr-2 py-2 bg-gray-100 rounded-md">
                     {/* title, maybe logo idk*/}
-                    <h1>vtm character vault</h1>
+                    <span>vtm character vault</span>
                     {/* search */}
                     <input type="text" placeholder="Search?" className="px-5 py-2 text-black text-xs text-right rounded-md transition hover:bg-gray-200 focus:outline-0" />
                 </div>     
