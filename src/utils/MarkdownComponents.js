419e9d7bb802d0190fb488f7dff248c32a630ed1
frantan <francinetaing13@gmail.com>
2025-12-24 00:09:49 +0700

    feat: create a shared custom renderer to handle links in markdown

---
+++ src/utils/MarkdownComponents.js
@@ -0,0 +1,20 @@
+// import react stuff
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// custom renderer for link (<a>) tags
+const LinkRenderer = ({ href, children }) => {
+  // check if link is internal or within the same site (starts with '/')
+  if (href.startsWith('/')) {
+    // if yes => use react-router <Link>
+    return <Link to={href}>{children}</Link>;
+  }
+  
+  // if link is external, use <a>
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>
+  );
+};
+
+// if you see <a> tag, use LinkRenderer instead
+export const customRenderers = { a: LinkRenderer, };
